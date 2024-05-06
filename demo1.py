65242aa14ca829abd8e5527b00c322e9de46b9a1
qingzhao0512 <qingzhao0512@gmail.com>
2024-05-06 18:16:09 +0800

    hw

--- demo1.py
+++ demo1.py
@@ -1,3 +1,11 @@
+'''
+Author: qingzhao0512 qingzhao0512@gmail.com
+Date: 2024-05-06 17:31:33
+LastEditors: qingzhao0512 qingzhao0512@gmail.com
+LastEditTime: 2024-05-06 17:34:03
+FilePath: \SeleniumTest\demo1.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -7,7 +15,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 browser = webdriver.Chrome()
 try:
     browser.get('https://www.baidu.com')
-    input = browser.find_element_by_id('kw')
+    # 使用 By.ID 替换 find_element_by_id
+    input = browser.find_element(By.ID, 'kw')
     input.send_keys('Python')
     input.send_keys(Keys.ENTER)
     wait = WebDriverWait(browser, 10)
--- demo2.py
+++ demo2.py
@@ -1,8 +1,16 @@
+'''
+Author: qingzhao0512 qingzhao0512@gmail.com
+Date: 2024-05-06 17:31:33
+LastEditors: qingzhao0512 qingzhao0512@gmail.com
+LastEditTime: 2024-05-06 17:44:49
+FilePath: \SeleniumTest\demo2.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 from selenium import webdriver
 
 browser = webdriver.Chrome()
 browser.get('https://www.taobao.com')
-input_first = browser.find_element_by_id('q')
+# input_first = browser.find_element_by_id('q')           # id定位，应该已经过期了
 input_second = browser.find_element_by_css_selector('#q')
 input_third = browser.find_element_by_xpath('//*[@id="q"]')
 print(input_first, input_second, input_third)
